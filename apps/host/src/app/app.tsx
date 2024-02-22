b0b217d00b8644f55991a89404db43f55ed31158
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2024-02-22 11:11:04 -0300

    feat: add routes to host

--- apps/host/src/app/app.tsx
+++ apps/host/src/app/app.tsx
@@ -3,6 +3,8 @@ import * as React from 'react';
 import NxWelcome from './nx-welcome';
 
 import { Link, Route, Routes } from 'react-router-dom';
+const Shop = React.lazy(() => import('shop/Module'));
+const Cart = React.lazy(() => import('cart/Module'));
 
 export function App() {
   return (
@@ -11,9 +13,17 @@ export function App() {
         <li>
           <Link to="/">Home</Link>
         </li>
+        <li>
+          <Link to="/shop">Shop</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
       </ul>
       <Routes>
         <Route path="/" element={<NxWelcome title="host" />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </React.Suspense>
   );
